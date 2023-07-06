b672ef92020c7950c1d21fb1fa80e80b264365aa
mahender-greyblue <mahender.pal@greyblue.in>
2023-07-06 17:27:00 +0530

    nav bar pending

--- assets/js/main.js
+++ assets/js/main.js
@@ -102,11 +102,25 @@ $list.addEventListener( "keyup", handleSlideKey );
 
 
 
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  } 
+
+
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
+
+
+
+
+
+// function myFunction() {
+//     var x = document.getElementById("myTopnav");
+//     if (x.className === "topnav") {
+//       x.className += " responsive";
+//     } else {
+//       x.className = "topnav";
+//     }
+//   } 
